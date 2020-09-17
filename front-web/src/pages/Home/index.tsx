107ebc536233b8c2aaf6a0a1c5c895dcb967aceb
Luiz Gustavo <luiz.gustavogz02@gmail.com>
2020-09-17 15:20:47 -0300

    Criando navegação com rotas

--- front-web/src/pages/Home/index.tsx
+++ front-web/src/pages/Home/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import './styles.css';
 import { ReactComponent as ArrowIcon } from '../..//assets/arrow.svg';
 import { ReactComponent as GamerImage } from '../..//assets/gamer.svg';
+import { Link } from 'react-router-dom';
+import './styles.css';
 
 const Home = () => (
     <div className="home-container">
@@ -12,14 +13,16 @@ const Home = () => (
             <h3 className="home-text-subtitle">
                 Clique no botão abaixo e saiba quais são os jogos que os gamers estão escolhendo!
             </h3>
-            <div className="home-actions">
-                <button className="home-btn">
-                    QUERO SABER QUAIS SÃO
-                </button>
-                <div className="home-btn-icon">
-                    <ArrowIcon />
+            <Link to="/records">
+                <div className="home-actions">
+                    <button className="home-btn">
+                        QUERO SABER QUAIS SÃO
+                    </button>
+                    <div className="home-btn-icon">
+                        <ArrowIcon />
+                    </div>
                 </div>
-            </div>
+            </Link>
         </div>
         <GamerImage className="home-image"/>
     </div>

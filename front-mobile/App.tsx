ce5b1994bf22dc443683b1f51c0db3714b08cf42
Luiz Gustavo <luiz.gustavogz02@gmail.com>
2020-09-19 16:20:07 -0300

    Adicionando página home no React native

---
+++ front-mobile/App.tsx
@@ -0,0 +1,33 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { useFonts, Play_400Regular, Play_700Bold } from '@expo-google-fonts/play';
+import Header from './src/components/Header';
+import Home from './src/pages/Home';
+import { AppLoading } from 'expo';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Play_400Regular,
+    Play_700Bold
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  } else {
+    return (
+      <View style={styles.container}>
+        <Header />
+        <Home />
+        <StatusBar style="light" />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#0B1F34',
+  }
+});
